4ba509ae64bddd0ebff802e8b081b4c617da1f1f
Teng Li <2344067l@student.gla.ac.uk>
2018-01-24 15:53:36 +0000

    End of Chapter 6

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,17 +1,38 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+
+def show_category(request, category_name_slug):
+	context_dic = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dic['pages'] = pages
+		context_dic['category'] = category
+	except Category.DoesNotExist:
+		context_dic['pages'] = None
+		context_dic['category'] = None
+
+	return render(request, 'rango/category.html', context_dic)
+
 def index(request):
     #return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
 	
 	#Construct a dictionary to pass to the template engin as its context
 	#Note the key boldmessage is the same as {{boldmessage}} in the template!
-	context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
+
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+
+	context_dict = {'categories' : category_list,
+					'pages': page_list}
 	
 	# Return a rendered response to send to the client
 	# We make use of the shortcut function to make our lives easier
 	# Note that the first parameter is the template we wish to use
-	return render(request, 'rango/index.html', context=context_dict)
+	return render(request, 'rango/index.html', context_dict)
 def about(request):
     #return HttpResponse("Rango says here is the about page.<br/><a href='/rango/about/'>Index</a>")
 	

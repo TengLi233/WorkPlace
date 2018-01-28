b220907737b64e56179af885a3e55d5af04b943d
Teng Li <2344067l@student.gla.ac.uk>
2018-01-28 19:26:22 +0000

    adjust some formats

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-                           help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -16,15 +15,9 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
-    class Meta:
-        model = Page
-        exclude = ('category',)
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -35,6 +28,10 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
             return cleaned_data
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
